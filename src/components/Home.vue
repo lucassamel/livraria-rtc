d23c3526469978bf1f15dec94ea73a67397703ce
Lucas Samel <lucas.samel97@gmail.com>
2021-07-30 21:22:36 -0300

    Alterando o consumo da api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,48 +1,62 @@
 <template>
   <div>
-    <br>
-    <div class="cards" v-if="allItens">
-      <b-card  :key="i.id" v-for="i in allItens"
-        :title="i.title"
-        :img-src="i.src"
+    <br />
+    <div class="cards" v-if="livros.length > 0">
+      <b-card
+        v-for="livro in livros"
+        :key="livro.id"
+        :title="livro.nome"
         :id="i.id"
         img-alt="Image"
         img-top
         tag="article"
         style="max-width: 20rem;"
         class="mb-2"
-        
       >
-        <b-card-text> {{i.descricao}} </b-card-text>
-        <b-card-text v-if=" (i.estoque) <= 2 " id="estoque">Estoque Disponível: {{ i.estoque}}</b-card-text>     
-        <b-card-text v-if=" (i.estoque) >= 3 " id="disponivel">Estoque Disponível: {{ i.estoque}}</b-card-text>    
+        <b-card-text> {{ livro.descricao }} </b-card-text>
+        <b-card-text v-if="livro.estoque <= 2" id="estoque"
+          >Estoque Disponível: {{ livro.estoque }}</b-card-text
+        >
+        <b-card-text v-if="livro.estoque >= 3" id="disponivel"
+          >Estoque Disponível: {{ livro.estoque }}</b-card-text
+        >
         <b-button size="sm" class="mb-2" variant="danger">
-          <b-icon @click="deleteIten(i.id)" icon="trash" ></b-icon> 
+          <b-icon @click="deleteIten(livro.id)" icon="trash"></b-icon>
         </b-button>
         <router-link
-           tag="a"
-           :to="{ name: 'DetalheIten', params: { id: i.id } }"
-       >
-        <b-button class="margemBotao" size="sm" variant="success"
-          >Detalhe do Item</b-button>  
-      </router-link>
-         
-          
+          tag="a"
+          :to="{ name: 'DetalheIten', params: { id: livro.id } }"
+        >
+          <b-button class="margemBotao" size="sm" variant="success"
+            >Detalhe do Item</b-button
+          >
+        </router-link>
       </b-card>
-      
     </div>
-
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+//import config from "../config/config";
+import axios from "axios";
+
+/*import { mapGetters, mapActions } from "vuex";
+ */
 
 export default {
-    name: "home",
-  components: {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      livros: [],
+    };
   },
-  computed: mapGetters(["allItens"]),
+  created() {
+    axios.get('http://localhost:61303/api/livros').then((response) => {
+      this.livros = response.data
+    });
+  },
+  /**computed: mapGetters(["allItens"]),
   created() {
     this.getItens();
   },
@@ -57,27 +71,27 @@ export default {
       this.$router.push({ name: 'DetalheIten' });
     }
   }
-}
+  */
+};
 </script>
 
 <style>
-.cards{
+.cards {
   justify-content: space-between;
   display: flex;
   flex-wrap: wrap;
 }
 
-.margemBotao{
+.margemBotao {
   margin: 5px;
   float: right;
 }
 
-#estoque{
+#estoque {
   color: red;
 }
 
-#disponivel{
+#disponivel {
   color: green;
 }
-
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './components/Home.vue'
+import Login from './components/Login'
+import Sobre from './components/Sobre'
+
+Vue.use(Router)
+
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        } ,
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/sobre',
+            name: 'sobre',
+            component: Sobre
+        }
+      
+    ]
+});
